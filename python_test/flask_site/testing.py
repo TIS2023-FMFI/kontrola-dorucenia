ad63fd1833a7bd95bdec055c8fadc2ca178d5e6b
Martines1 <slovak.m10@gmail.com>
2023-11-08 09:35:45 +0100

    pridana funkcionalita emailu

--- python_test/flask_site/testing.py
+++ python_test/flask_site/testing.py
@@ -1,17 +1,36 @@
-from flask import Flask
-import pandas as pd
-
+from flask import Flask, request, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 't402829@gmail.com'
+app.config['MAIL_PASSWORD'] = ''# heslo od emailu
+
+mail = Mail(app)
 
 @app.route('/')
-def test():
-    workbook = pd.read_excel('../example.xls')
-    rows = "Column First Name in excel document example.xls contains values : <br>"
-    for index,row in workbook.iterrows():
-        rows += row['First Name'] + ' <br> '
-    return rows
+def index():
+    #recipient_email = request.form['recipient_email']
+    subject = 'Hello from your Flask app!'
+    body = 'This is a test email sent from a Flask app.'
+
+    msg = Message(subject, sender='t402829@gmail.com', recipients=["t402829@gmail.com"])
+    msg.body = body
+
+    try:
+        mail.send(msg)
+        return 'Email sent successfully!'
+    except Exception as e:
+        return 'Error sending email: ' + str(e)
+    
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
