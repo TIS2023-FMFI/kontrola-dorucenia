b189f55c047d5f588e14fb87206273fa75eaec41
Martines1 <slovak.m10@gmail.com>
2023-11-28 18:50:10 +0100

    Pridané hašovanie údajov a parametrizovanie dotazov

--- src/functions.py
+++ src/functions.py
@@ -1,15 +1,14 @@
 import sqlite3
+from flask_bcrypt import Bcrypt
+
 
 def verify_user(mail, password):
-    
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    select = """SELECT * FROM user
-            WHERE email='{m}'
-            AND password='{p}'""".format(m = mail, p = password)
-    cursor.execute(select)
-    user = cursor.fetchall();
-    cursor.close()
-    connection.close()
-
-    return user
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        select = """SELECT * FROM user
+                        WHERE email=?"""
+        cursor.execute(select, (mail,))
+        fetch_user = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return fetch_user if fetch_user and Bcrypt().check_password_hash(fetch_user[4], password) else None
