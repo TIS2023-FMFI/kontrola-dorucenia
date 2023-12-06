06fbb0ff5897ebe04608ae960f511a1317f5e32d
unknown <drahovsky3@uniba.sk>
2023-12-06 21:42:33 +0100

    prva iteracia sablony

---
+++ website/authorization.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('mail')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        User.query.get(user.id)
+        if user:
+            if user.password == password:  # check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
