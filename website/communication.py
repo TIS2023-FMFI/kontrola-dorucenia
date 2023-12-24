c4d5d6e5133d24eb131b58b23636c20b601763c0
Martines1 <slovak.m10@gmail.com>
2023-12-24 13:36:00 +0100

    New functionality

--- website/communication.py
+++ website/communication.py
@@ -2,31 +2,33 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 import secrets
 import string
+from collections import OrderedDict
+
 
 communication = Blueprint('communication', __name__)
-conversations = {}
+conversations = OrderedDict()
 
-def generate_random_url():
+def generate_random_url() -> str:
     characters = string.ascii_letters + string.digits
     random_url = ''.join(secrets.choice(characters) for _ in range(20))
+    existing_urls = set(conversations.keys())
+    while random_url in existing_urls: # Ak by sa nahodou vytvorila url, ktora uz existuje
+        random_url = ''.join(secrets.choice(characters) for _ in range(20))
+    conversations[random_url] = []
     return random_url
 
-@communication.route('/chat/', methods=['GET', 'POST'])
-@login_required
-def chat():
-    if request.method == 'POST':
-        new_chat_id = generate_random_url()
-        conversations[new_chat_id] = []
-        return redirect(url_for('communication.create_chat', chat_id=new_chat_id))
+def getConversations() -> OrderedDict:
+    return conversations
 
-    existing_chats = conversations.keys()
-
-    return render_template('create_chat.html', existing_chats=existing_chats)
-
+def deleteConversation(index : int) -> bool:
+    if len(conversations) < index+1:
+        return False
+    toDelete = conversations.items()[index]
+    conversations.pop(toDelete, None)
+    return True 
 
 @communication.route('/<chat_id>', methods=['GET', 'POST'])
-@login_required
-def create_chat(chat_id):
+def chat(chat_id):
     if chat_id not in conversations:
         return render_template('chat.html', chat_id='err', messages="Site not found!")
 
@@ -34,12 +36,5 @@ def create_chat(chat_id):
         message = request.form['message']
         conversations[chat_id].append({'sender': current_user.name, 'message': message})
 
-    return render_template('chat.html', chat_id=chat_id, messages=conversations[chat_id])
+    return render_template('SK.html', chat_id=chat_id, messages=conversations[chat_id])
 
-
-@communication.route('/generate_new_chat', methods=['POST'])
-@login_required
-def generate_new_chat():
-    new_chat_id = generate_random_url()
-    conversations[new_chat_id] = []
-    return redirect(url_for('communication.create_chat', chat_id=new_chat_id))
