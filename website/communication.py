b86b59bb5ac30e073ff4978832b086b9f6e4680a
drahy333 <mattuss12@gmail.com>
2024-01-08 15:44:56 +0100

    pridanie zaznamenavania komunikacie medzi dispecermi

--- website/communication.py
+++ website/communication.py
@@ -3,38 +3,126 @@ from flask_login import login_required, current_user
 import secrets
 import string
 from collections import OrderedDict
+from . import db
 
+from website.models import Request, Response, Chat
 
 communication = Blueprint('communication', __name__)
 conversations = OrderedDict()
+
 
 def generate_random_url() -> str:
     characters = string.ascii_letters + string.digits
     random_url = ''.join(secrets.choice(characters) for _ in range(20))
     existing_urls = set(conversations.keys())
-    while random_url in existing_urls: # Ak by sa nahodou vytvorila url, ktora uz existuje
+    while random_url in existing_urls:  # Ak by sa nahodou vytvorila url, ktora uz existuje
         random_url = ''.join(secrets.choice(characters) for _ in range(20))
     conversations[random_url] = []
     return random_url
 
+
 def getConversations() -> OrderedDict:
     return conversations
 
-def deleteConversation(index : int) -> bool:
-    if len(conversations) < index+1:
+
+def deleteConversation(index: int) -> bool:
+    if len(conversations) < index + 1:
         return False
     toDelete = conversations.items()[index]
     conversations.pop(toDelete, None)
-    return True 
+    return True
+
 
 @communication.route('/<chat_id>', methods=['GET', 'POST'])
 def chat(chat_id):
-    if chat_id not in conversations:
+    current_response = Response.query.filter(Response.response_id == chat_id).all()
+    current_request = Request.query.filter(Request.response_id == chat_id).all()
+    loaded = False
+    if not current_response:
         return render_template('chat.html', chat_id='err', messages="Site not found!")
+    current_response = current_response[0]
+    current_request = current_request[0]
 
     if request.method == 'POST':
-        message = request.form['message']
-        conversations[chat_id].append({'sender': current_user.name, 'message': message})
 
-    return render_template('SK.html', chat_id=chat_id, messages=conversations[chat_id])
+        loaded_checkbox_value = request.form.get('loaded')
+        if loaded_checkbox_value == 'loaded' and not current_response.loaded:
+            #order = Order(current_request.order_code)
 
+            new_chat = Chat(string="Nákladka sa vykoná v dohodnutom čase.", response_id=current_response.response_id)
+            db.session.add(new_chat)
+            db.session.commit()
+            current_response.load()
+
+        unloaded_checkbox_value = request.form.get('unloaded')
+        if unloaded_checkbox_value == 'unloaded' and not current_response.unloaded:
+            new_chat = Chat(string="Výkladka sa vykoná v dohodnutom čase.", response_id=current_response.response_id)
+            db.session.add(new_chat)
+            db.session.commit()
+            current_response.unload()
+
+        cause_delay = request.form.get('cause_delay')
+        if cause_delay:
+            current_response.set_root_cause(cause_delay)
+
+        comment = request.form.get('message')
+        if comment:
+            current_response.set_comment(comment)
+
+        date = request.form.get('date')
+        time = request.form.get('time')
+
+        late_loading_value = request.form.get('late_loading')
+        if late_loading_value:
+            if not current_response.loaded:
+                current_response.set_loading_date(date)
+                current_response.set_loading_time(time)
+                current_response.set_delay_loading(True)
+
+                cause = get_root_cause(cause_delay)
+                if cause == 'Iný dôvod':
+                    cause = comment
+
+                new_chat = Chat(string="Vozidlo bude meškať na nákladku z dôvodu " + cause + ".",
+                                response_id=current_response.response_id)
+                db.session.add(new_chat)
+                db.session.commit()
+                new_chat = Chat(string="Predpokladaný čas nákladky: " + date + " " + time,
+                                response_id=current_response.response_id)
+                db.session.add(new_chat)
+                db.session.commit()
+
+        late_unloading_value = request.form.get('late_unloading')
+        if late_unloading_value:
+            if not current_response.unloaded:
+                current_response.set_unloading_date(date)
+                current_response.set_unloading_time(time)
+                current_response.delay_unloading = True
+
+                cause = get_root_cause(cause_delay)
+                if cause == 'Iný dôvod':
+                    cause = comment
+
+                new_chat = Chat(string="Vozidlo bude meškať na výkladku z dôvodu " + cause + ".", response_id=current_response.response_id)
+                db.session.add(new_chat)
+                db.session.commit()
+                new_chat = Chat(string="Predpokladaný čas výkladky: " + date + " " + time, response_id=current_response.response_id)
+                db.session.add(new_chat)
+                db.session.commit()
+    all_chat = Chat.query.filter(Chat.response_id == current_response.response_id).all()
+    return render_template('SK.html', chat_id=chat_id, res=current_response, req=current_request, chat=all_chat)
+
+
+def get_root_cause(case):
+    switch = {
+        'weather': 'Nepriaznivé počasie',
+        'broken_truck': 'Pokazený kamión',
+        'accident': 'Dopravná nehoda',
+        'delay_loading': 'Zdržanie na predchádzajúcej nákladke',
+        'delay_unloading': 'Zdržanie na predchádzajúcej výkladke',
+        'driver_performance': 'Výkon vodiča',
+        'traffic_situation': 'Dopravná situácia',
+        'other': 'Iný dôvod',
+    }
+
+    return switch.get(case, None)
--- website/requests.py
+++ website/requests.py
@@ -29,12 +29,22 @@ def create_request():
         send_time = datetime.time(int(timelist[0]), int(timelist[1]))
         additional_message = data.get('additional_message')
         response = False
-        new_request = Request(user_id=user_id, order_code=order_code, carrier_email=carrier_email, send_date=send_date, send_time=send_time, additional_message=additional_message, response=response )
-        db.session.add(new_request)
-        db.session.commit()
         new_chat_id = generate_random_url()
         main_page_url = url_for('views.home', _external=True)
         website_link = f'{main_page_url}/{new_chat_id}'
+
+        new_request = Request(user_id=user_id, order_code=order_code, carrier_email=carrier_email, send_date=send_date, send_time=send_time, additional_message=additional_message, response=response, response_id  = new_chat_id)
+        db.session.add(new_request)
+        db.session.commit()
+
+        new_response = Response(request_id = new_request.id, loaded = False, unloaded = False, response_id = new_chat_id)
+        db.session.add(new_response)
+        db.session.commit()
+
+        new_chat = Chat(string="Čaká sa na potvrdenie času nákladky.", response_id=new_response.response_id)
+        db.session.add(new_chat)
+        db.session.commit()
+
         request_data = {'id': new_request.id, 'order_code': new_request.order_code, 'carrier_email': new_request.carrier_email, 'send_date': new_request.send_date, 'send_time': json.dumps(new_request.send_time, default=str)}
         scheduled_date = datetime.datetime(int(datelist[0]), int(datelist[1]), int(datelist[2]), int(timelist[0]), int(timelist[1]), 0)
         scheduler.add_job(send_email, 'date', args=['Communication',  f'{website_link}\n{additional_message}', carrier_email], run_date=scheduled_date)
@@ -60,9 +70,17 @@ def delete_requests():
         request_ids_to_delete = data.get('requests_to_delete')
         for request_id in request_ids_to_delete:
             req = find_request(int(request_id))
-            deleteConversation(int(request_id)-1)
+            res = Response.query.filter(Response.request_id == request_id).all()[0]
             if not req.removeRequest():
                 return jsonify({'success': False})
+            if not res.removeResponse():
+                return jsonify({'success': False})
+
+            chats = Chat.query.filter(Chat.response_id == req.response_id).all()
+
+            for ch in chats:
+                if not ch.removeChat():
+                    return jsonify({'success': False})
         return jsonify({'success': True})
     return jsonify({'success': False})
 
