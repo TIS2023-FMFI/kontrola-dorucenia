4200dee4a3822b967f58a463c4860809ab0cf046
Martines1 <slovak.m10@gmail.com>
2024-02-11 13:15:38 +0100

    venv variables support

--- website/utils.py
+++ website/utils.py
@@ -1,7 +1,7 @@
 from flask_mail import Mail, Message
 from .models import *
 from . import *
-
+from .config import get_env
 # Find user by id or email
 def find_user(id: int = None, email: str = None):
     users_from_db = User.query.all()
@@ -19,18 +19,18 @@ def find_request(id: int = None):
     return False
 
 
-
 def send_email(subject: str, body: str, email: str):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = b'hs8b9e256co648'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = 't402829@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'Pbui rzbg sabm swot '  # heslo od emailu
+    key = get_env("SECRET_KEY")
+    app.config['SECRET_KEY'] = key.encode()
+    app.config['SQLALCHEMY_DATABASE_URI'] = get_env("SQLALCHEMY")
+    app.config['MAIL_SERVER'] = get_env("MAIL_SERVER")
+    app.config['MAIL_PORT'] = int(get_env("MAIL_PORT"))
+    app.config['MAIL_USE_TLS'] = bool(int(get_env("MAIL_USE_TLS")))
+    app.config['MAIL_USE_SSL'] = bool(int(get_env("MAIL_USE_SSL")))
+    app.config['MAIL_USERNAME'] = get_env("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = get_env("MAIL_PASSWORD")
     with app.app_context():
-        msg = Message(subject, sender='t402829@gmail.com', recipients=[email])
+        msg = Message(subject, sender=get_env("MAIL_USERNAME"), recipients=[email])
         msg.body = body
         return Mail(app).send(msg)
